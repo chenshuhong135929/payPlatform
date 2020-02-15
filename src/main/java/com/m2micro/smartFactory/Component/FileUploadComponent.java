30acc3bf6a63a5dcc137917250e3cdc80a3aaa5b
chenshuhong520 <1048369112@qq.com>
2020-02-15 16:20:32 +0800

    提交

--- src/main/java/com/m2micro/smartFactory/Component/FileUploadComponent.java
+++ src/main/java/com/m2micro/smartFactory/Component/FileUploadComponent.java
@@ -3,14 +3,11 @@ package com.m2micro.smartFactory.Component;
 import com.m2micro.smartFactory.bo.FileBo;
 import com.m2micro.smartFactory.utils.RandomUtils;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 @ConfigurationProperties(prefix = "fileuploadcomponent")
 @Component
 public class FileUploadComponent {
@@ -24,10 +21,12 @@ public class FileUploadComponent {
      * @param file
      * @return
      */
+/*
     public FileBo writeUploadFile(MultipartFile file, String module) {
         String filename = file.getOriginalFilename();
-        realpath = realpath +  module +"/";
-        File fileDir = new File(realpath);
+        String imageUrl  = realpath +  module +File.separator;
+
+        File fileDir = new File(imageUrl);
         if (!fileDir.exists())
             fileDir.mkdirs();
 
@@ -37,7 +36,7 @@ public class FileUploadComponent {
         FileOutputStream fos = null;
         try {
             input = file.getInputStream();
-            fos = new FileOutputStream(realpath + "/" + filename);
+            fos = new FileOutputStream(imageUrl + "/" + filename);
             IOUtils.copy(input, fos);
         } catch (Exception e) {
             e.printStackTrace();
@@ -53,6 +52,42 @@ public class FileUploadComponent {
 
         return fileBo;
     }
+*/
+
+
+    /**
+     * 文件上传处理
+     *
+     * @param multipartFile
+     * @return
+     */
+    public FileBo writeUploadFile(MultipartFile multipartFile, String module) {
+        String fileName = multipartFile.getOriginalFilename();
+        String imageUrl  = realpath +  module +File.separator;
+
+        File fileDir = new File(imageUrl);
+        if (!fileDir.exists())
+            fileDir.mkdirs();
+        String extname = FilenameUtils.getExtension(fileName);
+
+        fileName = Math.abs(multipartFile.getOriginalFilename().hashCode()) + RandomUtils.createRandomString( 4 ) + "." + extname;
+
+        try {
+            File file =  new File(imageUrl + "/" + fileName);
+            multipartFile.transferTo(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        FileBo fileBo = new FileBo();
+        fileBo.setFileName(multipartFile.getOriginalFilename());
+        fileBo.setFilePath(picture + module + "/" + fileName);
+
+        return fileBo;
+    }
+
+
+
 
     public String getRealpath() {
         return realpath;
